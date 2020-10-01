101526f990e6ca701b17a2b26ec62de984eef36d
Kiichiro-T <tennis_0503@outlook.jp>
2020-10-01 23:08:16 +0900

    payjpのフォームをカスタム

--- app/javascript/packs/components/material-ui/PayjpForm.jsx
+++ app/javascript/packs/components/material-ui/PayjpForm.jsx
@@ -2,42 +2,108 @@ import React from 'react';
 
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 
 class PayjpForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      open: false
+    };
+
     this.payjp = Payjp('pk_test_58e3a5f8f376dc6bc1b9c472')
-
-    // elementsを取得します。ページ内に複数フォーム用意する場合は複数取得ください
     this.elements = this.payjp.elements()
-
-    // 入力フォームを分解して管理・配置できます
     this.numberElement = this.elements.create('cardNumber')
     this.expiryElement = this.elements.create('cardExpiry')
     this.cvcElement = this.elements.create('cardCvc')
-
+    this.handleClickOpen = this.handleClickOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.checkCards = this.checkCards.bind(this);
+    this.cardChange = this.cardChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
 
   componentDidMount() {
-    this.numberElement.mount('#number-form')
-    this.expiryElement.mount('#expiry-form')
-    this.cvcElement.mount('#cvc-form')
+    const style = {
+      base: {
+        background: 'red',
+      },
+      invalid: {
+        color: 'red',
+      }
+    }
+    this.numberElement.mount('#number-form', {style: style})
+    this.expiryElement.mount('#expiry-form', {style: style})
+    this.cvcElement.mount('#cvc-form', {style: style})
   }
 
-  // ボタンが押されたらtokenを生成する関数を用意します
-  onSubmit () {
+  handleClickOpen() {
+    this.setState({
+      open: true
+    });
+  };
+
+  handleClose() {
+    this.setState({
+      open: false
+    });
+  };
+
+  onSubmit() {
     this.payjp.createToken(this.numberElement).then(function(r) {
       document.querySelector('#token').innerText = r.error ? r.error.message : r.id
     })
+  }
+
+  checkCards() {
+    this.payjp.retrieveAvailability().then((response) => {
+      if (response.error) {
+        // handle error
+      } else {
+        console.log(response)
+      }
+    })
+  }
+
+  cardChange(event) {
+    console.log(event)
   }
 
 
   render() {
     return (
       <>
-        <div id="number-form" className="payjs-outer"></div>
+        <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
+          支払い情報を入力
+        </Button>
+        <Dialog open={this.state.open} onClose={this.handleClose} aria-labelledby="form-dialog-title">
+          <DialogTitle id="form-dialog-title">支払い情報</DialogTitle>
+          <DialogContent>
+            {/* <div id="number-form" className="payjs-outer"></div>
+            <div id="expiry-form" className="payjs-outer"></div>
+            <div id="cvc-form" className="payjs-outer"></div> */}
+          </DialogContent>
+          <DialogActions>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.handleClose}>
+              キャンセル
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.handleClose}>
+              OK
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <div id="number-form" className="payjs-outer" onChange={this.cardChange}></div>
         <div id="expiry-form" className="payjs-outer"></div>
         <div id="cvc-form" className="payjs-outer"></div>
         <Button
@@ -47,6 +113,13 @@ class PayjpForm extends React.Component {
           トークン作成
         </Button>
         <span id="token"></span>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={this.checkCards}>
+          カード確認
+        </Button>
+        <p id="card-status"></p>
       </>
     );
   }
